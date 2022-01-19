66608c261d2a9a984f08b4ca6a157feb51adfa68
AidanJones4 <ajones@spartaglobal.com>
2022-01-19 13:29:21 +0000

    Corrected variable names and comments, list comprehension super clean

--- starship_project.py
+++ starship_project.py
@@ -1,6 +1,5 @@
 import pymongo
 import requests
-from pprint import pprint as pp
 
 client = pymongo.MongoClient()
 db = client['starwars']
@@ -8,28 +7,28 @@ db = client['starwars']
 db.starships.drop()
 
 
-def get_all_json(api_link):
+def get_all_jsons(api_link):
 
     """
     Reads API link and returns list of all JSON files associated with it through the "next" keyword
-    Returns list of JSONs
+    Returns full JSON file
     """
-    full_json = []
+    full_jsons = []
 
     while True:
         resp = requests.get(api_link).json()
-        full_json += resp["results"]
+        full_jsons += resp["results"]
         if resp["next"] is None:
             break
         api_link = resp["next"]
 
     print("Returned all JSON files...")
-    return full_json
+    return full_jsons
 
 
 def api_url_to_id(api_link):
     """
-    Helper function that takes in character API link and returns their name
+    Helper function that takes in character API link and returns their ID
     Returns: name of character
     """
 
@@ -38,28 +37,28 @@ def api_url_to_id(api_link):
     return pilot_id
 
 
-def update_json(json):
+def update_jsons(jsons):
     """
-    Loops through list of json files
+    Loops through JSON files
     Changes pilot data from API link to pilot ID
     Returns: Updated list of jsons
     """
 
-    for json_dict in json:
+    for json_dict in jsons:
         json_dict["pilots"] = [api_url_to_id(url) for url in json_dict["pilots"]]
 
     print("Pilot API url converted to ID...")
-    return json
+    return jsons
 
 
-def upload_json(json):
+def upload_jsons(jsons):
     """
-    Uploads each JSON element within list of JSONS one at a time into starships collection
+    Uploads each JSON file individually
     Creates starships collection if it doesn't already exist
     Returns: nothing
 
     """
-    for starship in json:
+    for starship in jsons:
         db.starships.insert_one(starship)
 
     print("Uploaded to MongoDB database.")
@@ -67,6 +66,6 @@ def upload_json(json):
 
 
 # Execution
-json = get_all_json("https://swapi.dev/api/starships")     # Get all JSONs
-id_json = update_json(json)                                # Update JSONs
-upload_json(id_json)                                       # Upload modified JSONs to starships collection
+starship_jsons = get_all_jsons("https://swapi.dev/api/starships")       # Get all JSONs
+id_jsons = update_jsons(starship_jsons)                                  # Update JSONs
+upload_jsons(id_jsons)                                                   # Upload modified JSONs to starships collection
