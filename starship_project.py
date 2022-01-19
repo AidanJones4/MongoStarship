9a9e5137322ef0f6e238a024b25408e6f6135813
AidanJones4 <ajones@spartaglobal.com>
2022-01-19 13:15:30 +0000

    Refactored, list comprehension super clean

--- starship_project.py
+++ starship_project.py
@@ -14,17 +14,17 @@ def get_all_json(api_link):
     Reads API link and returns list of all JSON files associated with it through the "next" keyword
     Returns list of JSONs
     """
-    list_of_jsons = []
+    full_json = []
 
     while True:
         resp = requests.get(api_link).json()
-        list_of_jsons += resp["results"]
+        full_json += resp["results"]
         if resp["next"] is None:
             break
         api_link = resp["next"]
 
-    print("List of all JSON files created...")
-    return list_of_jsons
+    print("Returned all JSON files...")
+    return full_json
 
 
 def api_url_to_id(api_link):
@@ -38,35 +38,35 @@ def api_url_to_id(api_link):
     return pilot_id
 
 
-def update_json(list_of_jsons):
+def update_json(json):
     """
     Loops through list of json files
     Changes pilot data from API link to pilot ID
     Returns: Updated list of jsons
     """
 
-    for json_dict in list_of_jsons:
+    for json_dict in json:
         json_dict["pilots"] = [api_url_to_id(url) for url in json_dict["pilots"]]
 
-    print("Pilot API links changed to character name...")
-    return list_of_jsons
+    print("Pilot API url converted to ID...")
+    return json
 
 
-def upload_jsons(list_of_jsons):
+def upload_json(json):
     """
     Uploads each JSON element within list of JSONS one at a time into starships collection
-    Creates starships collection if doesn't already exist
+    Creates starships collection if it doesn't already exist
     Returns: nothing
 
     """
-    for starship in list_of_jsons:
+    for starship in json:
         db.starships.insert_one(starship)
 
-    print("Uploaded to MongoDB database")
+    print("Uploaded to MongoDB database.")
     return None
 
 
 # Execution
-jsons = get_all_json("https://swapi.dev/api/starships")     # Get all JSONs
-id_json = update_json(jsons)                                # Update JSONs
-upload_jsons(id_json)                                       # Upload modified JSONs to starships collection
+json = get_all_json("https://swapi.dev/api/starships")     # Get all JSONs
+id_json = update_json(json)                                # Update JSONs
+upload_json(id_json)                                       # Upload modified JSONs to starships collection
